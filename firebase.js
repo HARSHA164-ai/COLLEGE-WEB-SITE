3cb8411cfb74e75b9f8bc40dc2ef518eb37b6823
HARSHA164-ai <bhuvansrisaiharsha07@gmail.com>
2025-05-30 21:57:21 +0530

    new

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_wWNnD_1pvZuahffwDa8hCDje2Aru3B8",
+    authDomain: "harshacollegewebsite.firebaseapp.com",
+    projectId: "harshacollegewebsite",
+    storageBucket: "harshacollegewebsite.firebasestorage.app",
+    messagingSenderId: "568331032925",
+    appId: "1:568331032925:web:725cf04c75452c06ed70b4"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
