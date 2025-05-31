091e69d5c293c9f002000d1ff63dba05824e1ed3
HARSHA164-ai <bhuvansrisaiharsha07@gmail.com>
2025-05-31 19:02:44 +0530

    BYE

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
+
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -12,6 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
